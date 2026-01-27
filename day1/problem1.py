3ec554ff9c1c35796f67dbbc7e61f7737ad46df3
tarun10898 <tarun.pondreti@gmail.com>
2026-01-27 17:28:25 +0530

    feat: problem 1

---
+++ day1/problem1.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+    int_value: int = int(input())
+    str_value: str = input()
+    bool_value:bool = bool(input())
+    float_value:float = float(input())
+    char_value: str =input()
+    int_values_2:int = int(input())
+    string_value: str = input()
+
+    print(f"interf=ger variable: {int_value}")
+    print(f"string value: {str_value}")
+    print(f"boolena value {bool_value}")
+    print(f"float_value: {float_value}")
+   
